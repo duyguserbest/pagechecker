e29491091880159fc9f630353ce512f07e0dd270
Duygu Serbest <serbestduygu@gmail.com>
2016-05-13 10:10:59 +0300

    pickle.dump was not able to open the file properly fixed by changing file openning mode.

--- pagechecker.py
+++ pagechecker.py
@@ -8,22 +8,25 @@ import os.path
 def job():
 
 	urls = ["http://fbe.gazi.edu.tr/posts?type=news", "http://www.cs.hacettepe.edu.tr/lisansustu/", "http://www.cs.hacettepe.edu.tr/lisansustu/duyuru.html", "http://yyegm.meb.gov.tr/www/duyurular/kategori/2"]
-	isFirstIteration = True
+
+	is_first_iteration = True
 	if os.path.exists("pagecheckerhash"):
-	    isFirstIteration = False
+	    is_first_iteration = False
 
-	hash_file = open("pagecheckerhash", "w+")
 
-	if not isFirstIteration:
+	if not is_first_iteration:
+	    hash_file = open("pagecheckerhash", "rb")
 	    local_hashes = pickle.load(hash_file)
+	    hash_file.close()
 	else:
-	    local_hashes = []
+		local_hashes = ["" for x in range(len(urls))]
 	    
 	for i in range(0, len(urls)):
 	    remote_data = urllib.urlopen(urls[i]).read()
 	    remote_hash = hashlib.md5(remote_data).hexdigest()
-	    print len(local_hashes)
-	    if not isFirstIteration:
+
+
+	    if not is_first_iteration:
 	        local_hash = local_hashes[i]
 	        if remote_hash == local_hash:
 	            print urls[i] + ' is NOT changed.'
@@ -32,14 +35,14 @@ def job():
 	    else:
 	        print "Created page hash for " + urls[i]
 
-	    local_hashes.append(remote_hash)
+	    local_hashes[i] = remote_hash
 	    
+	hash_file = open("pagecheckerhash", "wb+")
 	pickle.dump(local_hashes, hash_file)
-
 	hash_file.close()
 
 job()
-schedule.every().day.at("10:30").do(job)
+schedule.every().day.at("11:04").do(job)
 schedule.every().day.at("17:15").do(job)
 schedule.every().day.at("19:30").do(job)
 
